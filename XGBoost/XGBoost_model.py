bca5e3cb6a6198470e5869703aa46d27a7fe6b71
Felix Schulz <fhw.schulz@outlook.com>
2025-03-10 18:29:37 +0100

    Started the XGBoosted Tree

---
+++ XGBoost/XGBoost_model.py
@@ -0,0 +1,44 @@
+import numpy as np
+from xgboost import XGBClassifier
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc
+
+# Loading the Data
+X_train = np.load('../data/X_train.npy')
+y_train = np.load('../data/y_train.npy')
+
+X_test = np.load('../data/X_test.npy')
+y_test = np.load('../data/y_test.npy')
+
+xgb = XGBClassifier(n_estimators = 100,
+                     max_depth = 3,
+                     objective = 'binary:logistic'
+                     )
+
+xgb.fit(X_train, y_train)
+
+y_pred = xgb.predict(X_test)
+
+fpr = {}
+tpr = {}
+roc_auc = {}
+
+for i in range(4):
+    fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_pred[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
+
+plt.figure()
+
+classes = ['Star-Forming', 'Composite', 'AGN', 'LINER']
+
+for i in range(4):
+    plt.plot(fpr[i], tpr[i], lw=2, label=f'{classes[i]} (AUC = {roc_auc[i]:.2f})')
+
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve')
+
+plt.legend(loc='lower right')
+plt.show()
